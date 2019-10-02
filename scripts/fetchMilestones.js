5279557992e4f45ebea899b58be66d1252dd51ab
Austin Parker <austin@lightstep.com>
2019-10-02 15:31:56 -0400

    fix logic on script; should always pick lowest version

--- scripts/fetchMilestones.js
+++ scripts/fetchMilestones.js
@@ -3,47 +3,51 @@ import YAML from 'yaml'
 import fetch from 'node-fetch'
 const token = process.env.GH_TOKEN
 const file = fs.readFileSync('./data/progress_source.yaml', 'utf8')
-const data = YAML.parse(file)
+const sourceFileData = YAML.parse(file)
 
-const versions = data.columns.map(x => x.label)
+const versions = sourceFileData.columns.map(x => x.label)
+const labelData = sourceFileData.data
 
-async function getAllMilestones() {
-  for (const x of data.data) {
-    const baseUrl = x.sourceUrl.substring(19)
-    const apiUrl = `https://api.github.com/repos/${baseUrl}/milestones`
-    await fetch(apiUrl, {headers: { 'Authorization': `token ${token}`}}).then(res => res.json()).then(body => {
-      let matches = []
-      if (body.length > 1) {
-        body.filter(i => versions.some(v => {
-          if (i.title) {
-            if (i.title.includes(v)) {
-              i.msLabel = v
-              matches.push(i)
-            }
-          }
-        }))
-      } else {
-        if (body.title) {
-          versions.some(v => {
-            if (body.title.includes(v))
-            {
-              body.msLabel = v
-              matches.push(body)
-            }
-          })
-        }
+function checkIfMilestoneMatchesVersion(version, milestone) {
+  if (milestone.title && milestone.title.toLowerCase().includes(version)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function updateDataWithMilestoneInfo(data, milestoneLabel, milestone) {
+  data.msLabel = milestoneLabel
+  data.openIssues = milestone.open_issues
+  data.closedIssues = milestone.closed_issues
+  if (data.openIssues + data.closedIssues === 0) {
+    data.totalIssues = 0
+  } else {
+    data.totalIssues = (data.openIssues + data.closedIssues).toFixed(1)
+  }
+}
+
+async function fetchMilestoneDataForData(data) {
+  const baseUrl = data.sourceUrl.substring(19)
+  const apiUrl = `https://api.github.com/repos/${baseUrl}/milestones`
+  const response = await fetch(apiUrl, { headers: { 'Authorization': `token ${token}` }})
+  const resJson = await response.json()
+  return resJson
+}
+
+function findMilestone(data, milestones) {
+  for (const version of versions) {
+    let found = false
+    for (let ms of milestones) {
+      if (checkIfMilestoneMatchesVersion(version, ms)) {
+        updateDataWithMilestoneInfo(data, version, ms)
+        found = true
+        break;
       }
-      if (matches.length > 0) {
-        x.msLabel = matches[0].msLabel
-        x.openIssues = matches[0].open_issues
-        x.closedIssues = matches[0].closed_issues
-        if (x.openIssues + x.closedIssues === 0) {
-          x.totalIssues = 0
-        } else {
-          x.totalIssues = (matches[0].open_issues + matches[0].closed_issues).toFixed(1)
-        }
-      }
-    }).catch(err => console.error(err))
+    }
+    if (found) {
+      break;
+    }
   }
 }
 
@@ -53,8 +57,13 @@ async function main() {
     console.error("please set GH_TOKEN, exiting")
     process.exit(1)
   }
-  await getAllMilestones()
-  const finalYaml = YAML.stringify(data)
+
+  for (const datum of labelData) {
+    await fetchMilestoneDataForData(datum)
+      .then(msData => findMilestone(datum, msData))
+  } 
+
+  const finalYaml = YAML.stringify(sourceFileData)
   fs.writeFileSync('./data/progress_generated.yaml', finalYaml)
 }
 
