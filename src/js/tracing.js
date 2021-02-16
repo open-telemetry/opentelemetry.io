8c3d0730704edf4368627c0da15a65b879dc8ec9
Austin Parker <austin@lightstep.com>
2021-02-16 17:02:58 -0500

    Update Collector Exporter (#408)
    * update collector exporter
    
    * update to newest js api

--- src/js/tracing.js
+++ src/js/tracing.js
@@ -1,12 +1,14 @@
 import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { WebTracerProvider } from '@opentelemetry/web';
 import { DocumentLoad } from '@opentelemetry/plugin-document-load';
-import { CollectorExporter } from '@opentelemetry/exporter-collector';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 
-const exporter = new CollectorExporter({
+const exporter = new CollectorTraceExporter({
   serviceName: 'opentelemetry.io',
   url: 'https://otelwebtelemetry.com/v1/trace'
 })
+
 
 const locale = {
   "browser.language": navigator.language,
@@ -14,11 +16,15 @@ const locale = {
 }
 
 const provider = new WebTracerProvider({
-  plugins: [
+  resource: locale
+});
+
+registerInstrumentations({
+  instrumentations: [
     new DocumentLoad()
   ],
-  defaultAttributes: locale
-});
+  tracerProvider: provider
+})
 
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 provider.register();
