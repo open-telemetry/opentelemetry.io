01aaed7c5db1c050f9c415a13b1777ac2ebdf847
JustWPH <WPH95@users.noreply.github.com>
2021-02-17 21:46:10 +0800

    improve homepage scroll logic (#413)

--- src/js/base.js
+++ src/js/base.js
@@ -14,85 +14,77 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 const tracer = require('./tracing');
-(function($) {
+(function ($) {
 
-    'use strict';
+  'use strict';
 
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip();
-        $('[data-toggle="popover"]').popover();
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
 
-        $('.popover-dismiss').popover({
-            trigger: 'focus'
-        })
-    });
+    $('.popover-dismiss').popover({
+      trigger: 'focus'
+    })
+  });
 
 
-    function bottomPos(element) {
-        return element.offset().top + element.outerHeight();
+  function bottomPos(element) {
+    return element.offset().top + element.outerHeight();
+  }
+
+  // Bootstrap Fixed Header
+  $(function () {
+    var promo = $(".js-td-cover");
+    if (!promo.length) {
+      return
     }
 
-    // Bootstrap Fixed Header
-    $(function() {
-        var promo = $(".js-td-cover");
-        if (!promo.length) {
-            return
-        }
+    const announcementThreshold = Math.ceil($('section#announcement').outerHeight()) || -1;
+    // announcement_size + td-cover-block-0.padding-top/128px + img.padding-top/16 - nav.height/64px
+    const threshold = announcementThreshold + 128 + 16 - 64;
+    const onScroll = () => {
+      var navbarOffset = $('.js-navbar-scroll').offset().top;
+      if (navbarOffset > threshold) {
+        $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
+      } else {
+        $('.js-navbar-scroll').removeClass('navbar-bg-onscroll');
+        $('.js-navbar-scroll').addClass('navbar-bg-onscroll--fade');
+      }
 
-        var promoOffset = bottomPos(promo);
-        var navbarOffset = $('.js-navbar-scroll').offset().top;
+      var pageOffset = window.pageYOffset || document.documentElement.scrollTop;
+      if (pageOffset < announcementThreshold) {
+        $('.js-navbar-scroll').addClass('navbar-position-absolute').css("top", `${announcementThreshold}px`);
+      } else {
+        $('.js-navbar-scroll').removeClass('navbar-position-absolute').css("top", "0");
+      }
+    }
 
-        var threshold = Math.ceil($('.js-navbar-scroll').outerHeight());
-        if ((promoOffset - navbarOffset) < threshold) {
-            $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
-        }
-
-        var announcementThreshold = Math.ceil($('section#announcement').outerHeight()) || -1;
-        if (announcementThreshold > 0){
-            $('.js-navbar-scroll').addClass('navbar-position-absolute').css("top", `${announcementThreshold}px`);
-        }
-
-
-        $(window).on('scroll', function() {
-            var navtop = $('.js-navbar-scroll').offset().top - $(window).scrollTop();
-            var promoOffset = bottomPos($('.js-td-cover'));
-            var navbarOffset = $('.js-navbar-scroll').offset().top;
-            if ((promoOffset - navbarOffset) < threshold) {
-                $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
-            } else {
-                $('.js-navbar-scroll').removeClass('navbar-bg-onscroll');
-                $('.js-navbar-scroll').addClass('navbar-bg-onscroll--fade');
-            }
-
-            var pageOffset = window.pageYOffset || document.documentElement.scrollTop;
-            if (pageOffset < announcementThreshold) {
-                $('.js-navbar-scroll').addClass('navbar-position-absolute').css("top", `${announcementThreshold}px`);
-            } else {
-                $('.js-navbar-scroll').removeClass('navbar-position-absolute').css("top", "0");
-            }
-        });
+    onScroll()
+    $(window).on('scroll', function () {
+      onScroll()
     });
+  });
 
-    var Search = {
-        init: function() {
-            $(document).ready(function() {
-               $(document).on('keypress', '.td-search-input', function(e) {
-                    if (e.keyCode !== 13) {
-                        return
-                    }
+  var Search = {
+    init: function () {
+      $(document).ready(function () {
+        $(document).on('keypress', '.td-search-input', function (e) {
+          if (e.keyCode !== 13) {
+            return
+          }
 
-                    var query = $(this).val();
-                    var searchPage = window.location.origin + "/search/?q=" + query;
-                    document.location = searchPage;
+          var query = $(this).val();
+          var searchPage = window.location.origin + "/search/?q=" + query;
+          document.location = searchPage;
 
-                    return false;
-                });
+          return false;
+        });
 
-            });
-        },
-    };
+      });
+    },
+  };
 
-    Search.init();
+  Search.init();
 
 
 }(jQuery));
