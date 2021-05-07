e775113650f0bda2c69eb62e677741174d293141
Austin Parker <austin@lightstep.com>
2021-05-07 12:10:48 -0400

    update instrumentation deps

--- src/js/tracing.js
+++ src/js/tracing.js
@@ -1,7 +1,7 @@
 import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { WebTracerProvider } from '@opentelemetry/web';
-import { DocumentLoad } from '@opentelemetry/plugin-document-load';
-import { UserInteractionPlugin } from '@opentelemetry/plugin-user-interaction';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 import { Resource } from '@opentelemetry/resources';
@@ -24,8 +24,8 @@ const provider = new WebTracerProvider({
 
 registerInstrumentations({
   instrumentations: [
-    new DocumentLoad(),
-    new UserInteractionPlugin(),
+    new DocumentLoadInstrumentation(),
+    new UserInteractionInstrumentation(),
   ],
   tracerProvider: provider
 })
