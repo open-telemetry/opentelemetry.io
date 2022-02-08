4954d4227ad6ef342cfd6fe0e24c71833334f10c
Patrice Chalin <chalin@users.noreply.github.com>
2022-02-08 11:06:29 -0500

    cp-spec-pages.sh: use git clean rather than rm (#1108)

--- scripts/cp-spec-pages.sh
+++ scripts/cp-spec-pages.sh
@@ -4,13 +4,16 @@ BASE_DIR=$(dirname $0)
 SPEC=content-modules/opentelemetry-specification/specification
 DEST=content/en/docs/reference/specification
 
-rm -Rf $DEST/[^_]*
+git clean -xdf $DEST > /dev/null
 
 cp -R $SPEC/* $DEST/
 
 find $DEST/ -name "README.md" -exec sh -c 'f="{}"; mv -- "$f" "${f%README.md}_index.md"' \;
 
-FILES=`find $DEST -name "*.md" | grep -ve 'specification/_index.md$'`
+FILES=$(find $DEST -name "*.md" \
+  -not -path '*/specification/_index.md' \
+)
+
 $BASE_DIR/adjust-spec-pages.pl $FILES
 
 echo "Specification pages copied and processed."
