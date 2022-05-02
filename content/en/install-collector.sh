1a726826f658e4fcf48606b570716f136a1de30e
svrnm <neumanns@cisco.com>
2022-05-02 21:08:38 +0200

    Add collector installation script

---
+++ content/en/install-collector.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo -n "Installing the OpenTelemetry Collector into your current directory: $(pwd) in 3"
+#sleep 1
+#echo -n " 2"
+#sleep 1
+#echo -n " 1"
+#sleep 1
+echo ""
+
+LATEST_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/open-telemetry/opentelemetry-collector-releases/releases/latest)
+
+VERSION=${LATEST_URL##*/v}
+
+case "$(uname -s)" in
+   Darwin)
+     SUFFIX="darwin_amd64.tar.gz"
+     # SUFFIX="darwin_arm64.tar.gz"
+     ;;
+   Linux)
+     SUFFIX="linux_amd64.tar.gz"
+     # SUFFIX="linux_arm64.tar.gz"
+     ;;
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     SUFFIX="linux_amd64.tar.gz"
+     ;;
+   # Add here more strings to compare
+   # See correspondence table at the bottom of this answer
+   *)
+     echo "Your operating system is not supported, please compile the collector following these instructions: http://localhost:8888/docs/collector/getting-started/#local"
+     exit -1
+     ;;
+esac
+
+URL="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${VERSION}/otelcol-contrib_${VERSION}_${SUFFIX}"
+FILE="otelcol-contrib_${VERSION}_${SUFFIX}"
+
+echo " * Downloading ${URL}"
+
+curl -L -o "${FILE}" "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${VERSION}/otelcol-contrib_${VERSION}_${SUFFIX}"
+
+echo " * Unpacking ${FILE}"
+
+tar xvfz ${FILE}
