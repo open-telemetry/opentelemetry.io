a712c95ca8c5187e9c265acc3f2f0123a24da4af
Patrice Chalin <chalin@users.noreply.github.com>
2022-06-29 13:00:24 -0400

    Update adjust-spec-pages: avoid hard-coded front matter (#1493)

--- scripts/adjust-spec-pages.pl
+++ scripts/adjust-spec-pages.pl
@@ -14,6 +14,7 @@ my $gD = 0;
 my $specRepoUrl = 'https://github.com/open-telemetry/opentelemetry-specification';
 my $semConvRef = "$specRepoUrl/blob/main/semantic_conventions/README.md";
 my $path_base_for_github_subdir = 'content/en/docs/reference/specification';
+my $removeOnNextUpdate = 1; # TODO: remove all lines that use this variable on the next spec update
 
 my $rootFrontMatterExtra = <<"EOS";
 no_list: true
@@ -29,18 +30,18 @@ sub printTitleAndFrontMatter() {
   print "---\n";
   my $titleMaybeQuoted = ($title =~ ':') ? "\"$title\"" : $title;
   print "title: $titleMaybeQuoted\n";
-  print "weight: 1\n" if $title eq "Overview";
   ($linkTitle) = $title =~ /^OpenTelemetry (.*)/;
-  $linkTitle = 'FaaS' if $ARGV =~ /faas-metrics.md$/;
-  $linkTitle = 'HTTP' if $ARGV =~ /http-metrics.md$/;
-  print "linkTitle: $linkTitle\n" if $linkTitle;
-  print $rootFrontMatterExtra if $ARGV =~ /specification._index/;
+  $linkTitle = 'FaaS' if $ARGV =~ /faas-metrics.md$/ && $removeOnNextUpdate;
+  $linkTitle = 'HTTP' if $ARGV =~ /http-metrics.md$/ && $removeOnNextUpdate;
+  print "linkTitle: $linkTitle\n" if $linkTitle and $frontMatterFromFile !~ /linkTitle: /;
+  print $rootFrontMatterExtra if $ARGV =~ /specification._index/ && $removeOnNextUpdate;
+  print "$frontMatterFromFile" if $frontMatterFromFile;
+  print "weight: 1\n" if $title eq "Overview" && $removeOnNextUpdate;
   if ($ARGV =~ /specification.(.*?)_index.md$/) {
     print "path_base_for_github_subdir:\n";
     print "  from: $path_base_for_github_subdir/$1_index.md\n";
     print "  to: $1README.md\n";
   }
-  print "$frontMatterFromFile" if $frontMatterFromFile;
   print "---\n";
 }
 
