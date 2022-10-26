19d2e219125f29a0cb3c859a5974a091cc35504c
svrnm <neumanns@cisco.com>
2022-10-26 21:54:21 +0200

    Add ruby & js scraper
    Signed-off-by: svrnm <neumanns@cisco.com>

--- scripts/registry-scanner/index.mjs
+++ scripts/registry-scanner/index.mjs
@@ -1,19 +1,41 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import  { promises as fs} from 'fs'
 import { Octokit } from "@octokit/rest";
 import { URL } from 'url';
 
-const auth = process.env.GITHUB_AUTH_TOKEN;
+// Before running this script set the GITHUB_AUTH_TOKEN as an environment variable,
+// e.g.
+// $ export GITHUB_AUTH_TOKEN=ghp_YOUR_TOKEN_HERE
+// $ node index.mjs
+const octokit = new Octokit({auth: process.env.GITHUB_AUTH_TOKEN});
 
-const octokit = new Octokit({auth});
+// Please uncomment the entries you'd like to scan for.
+// ['receiver','exporter','processor'].forEach(async (component) => await scrapeCollectorComponent(component))
+// scrapeInstrumentationLibrariesByLanguage('js', 'plugins/node')
+scrapeInstrumentationLibrariesByLanguage('ruby', 'instrumentation')
 
 async function scrapeNew(path, repo, filter = () => true, keyMapper = x => x, owner = 'open-telemetry') {
     const result = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
         owner,
         repo,
         path
-    })  
+    })
     return result.data.reduce((carry, current) => {
-        if(filter(current)) {
+        if(filter(current) && current.type === 'dir') {
             const key = keyMapper(current.name)
             carry[key] = current
         }
@@ -33,16 +55,16 @@ async function scrapeExisting(type, noDash = false) {
     return result
 }
 
-function createMarkDown(title, registryType, repo, description) {
+function createMarkDown(shortName, title, language, registryType, repo, description) {
     return `---
 title: ${title}
 registryType: ${registryType}
 isThirdParty: false
-language: collector
+language: ${language}
 tags:
-    - go
+    - ${shortName}
     - ${registryType}
-    - collector
+    - ${language}
 repo: ${repo}
 license: Apache 2.0
 description: ${description}
@@ -52,17 +74,12 @@ otVersion: latest
 `
 }
 
-async function scrapeCollectorComponent(component) {
-    const filter = (item) => item.name.endsWith(component)
-    const keyMapper = (name) => name.substring(0, name.length - component.length)
-    const coreAndContrib = Object.assign(await scrapeNew(component, 'opentelemetry-collector', filter, keyMapper), await scrapeNew(component, 'opentelemetry-collector-contrib', filter, keyMapper))
-    const existing = await scrapeExisting(`collector-${component}`, true)
-
-    // Create New Entries
-    const result = Object.keys(coreAndContrib).reduce(async (carry, currentKey) => {
+async function createFilesFromScrapeResult(existing, found, settings) {
+    const { language, registryType } = settings
+    const result = Object.keys(found).forEach(async (currentKey) => {
         // Check if the entry does not exist already and create it if needed.
         if(!Object.keys(existing).includes(currentKey)) {
-            const current = coreAndContrib[currentKey]
+            const current = found[currentKey]
             const result = await octokit.request(`GET ${(new URL(current.url).pathname)}/README.md`)
             const readme = Buffer.from(result.data.content, 'base64').toString();
             const parsedReadme = readme.split('\n').reduce((result,line) => {
@@ -70,14 +87,33 @@ async function scrapeCollectorComponent(component) {
                     result.name = line.substring(1).trim() 
                 } 
                  if (!result.description && /^[A-Za-z]/.test(line)) { result.description = line  } return result }, {})
-            const md = createMarkDown(parsedReadme.name, component, current.html_url, parsedReadme.description)
-            carry[currentKey] = await fs.writeFile(`collector-${component}-${currentKey}.md`, md);
+            const md = createMarkDown(currentKey, parsedReadme.name, language, registryType, current.html_url, parsedReadme.description)
+            // collector entries are named reverse (collector-{registryTpe}) compared to languages ({registryTpe}-{language}), we fix this here.
+            const fileName = language === 'collector' ? `${language}-${registryType}-${currentKey}.md` : `${registryType}-${language}-${currentKey}.md`
+            await fs.writeFile(fileName, md)
         }
-        return carry
-    }, [])
-    return result
-
-    // To be done: Delete Outdated Entries
+    })
 }
 
-['receiver','exporter','processor'].forEach(async (component) => await scrapeCollectorComponent(component))
+async function scrapeInstrumentationLibrariesByLanguage(language, path) {
+    // https://github.com/open-telemetry/opentelemetry-js-contrib/tree/main/plugins/node/
+    const keyMapper = x => x.split('-').filter(y => !['opentelemetry', 'instrumentation'].includes(y)).join('-')
+    const found = await scrapeNew(path, `opentelemetry-${language}-contrib`, () => true, keyMapper)
+    const existing = scrapeExisting(`instrumentation-${language}`)
+    createFilesFromScrapeResult(existing, found, {
+        language,
+        registryType: 'instrumentation'
+    })
+}
+
+async function scrapeCollectorComponent(component) {
+    const filter = (item) => item.name.endsWith(component)
+    const keyMapper = (name) => name.substring(0, name.length - component.length)
+    const found = Object.assign(await scrapeNew(component, 'opentelemetry-collector', filter, keyMapper), await scrapeNew(component, 'opentelemetry-collector-contrib', filter, keyMapper))
+    const existing = await scrapeExisting(`collector-${component}`, true)
+    createFilesFromScrapeResult(existing, found, {
+        language: 'collector',
+        registryType: component
+    })
+}
+
