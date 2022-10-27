a6d9a4111384907523f35bcf3f9c2e8f97bf4dd3
svrnm <neumanns@cisco.com>
2022-10-27 10:46:01 +0200

    Fix bugs, add python
    Signed-off-by: svrnm <neumanns@cisco.com>

--- scripts/registry-scanner/index.mjs
+++ scripts/registry-scanner/index.mjs
@@ -26,7 +26,8 @@ const octokit = new Octokit({auth: process.env.GITHUB_AUTH_TOKEN});
 // Please uncomment the entries you'd like to scan for.
 // ['receiver','exporter','processor'].forEach(async (component) => await scrapeCollectorComponent(component))
 // scrapeInstrumentationLibrariesByLanguage('js', 'plugins/node')
-scrapeInstrumentationLibrariesByLanguage('ruby', 'instrumentation')
+// scrapeInstrumentationLibrariesByLanguage('ruby', 'instrumentation')
+scrapeInstrumentationLibrariesByLanguage('python', 'instrumentation', 'rst')
 
 async function scrapeNew(path, repo, filter = () => true, keyMapper = x => x, owner = 'open-telemetry') {
     const result = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
@@ -52,7 +53,6 @@ async function scrapeExisting(type, noDash = false) {
         }
         return carry
     }, {})
-    return result
 }
 
 function createMarkDown(shortName, title, language, registryType, repo, description) {
@@ -74,19 +74,38 @@ otVersion: latest
 `
 }
 
+function parseReadme(readme, format = 'md') {
+    if(format === 'rst') {
+        return readme.split('\n').reduce((result, line, index) => {
+            if (!result.name && index === 0) {
+                result.name = line
+            }
+            if (!result.description && index > 1 && /^[A-Za-z]/.test(line)) {
+                result.description = line
+            }
+            return result
+        }, {})
+    }
+    // Markdown (md) is the default
+    return readme.split('\n').reduce((result, line) => {
+        if (!result.name && line.startsWith('#')) {
+            result.name = line.substring(1).trim()
+        }
+        if (!result.description && /^[A-Za-z]/.test(line)) {
+            result.description = line
+        }
+        return result
+    }, {})    
+}
+
 async function createFilesFromScrapeResult(existing, found, settings) {
-    const { language, registryType } = settings
+    const { language, registryType, readmeFormat } = settings
     const result = Object.keys(found).forEach(async (currentKey) => {
         // Check if the entry does not exist already and create it if needed.
         if(!Object.keys(existing).includes(currentKey)) {
             const current = found[currentKey]
-            const result = await octokit.request(`GET ${(new URL(current.url).pathname)}/README.md`)
-            const readme = Buffer.from(result.data.content, 'base64').toString();
-            const parsedReadme = readme.split('\n').reduce((result,line) => {
-                 if(!result.name && line.startsWith('#')) { 
-                    result.name = line.substring(1).trim() 
-                } 
-                 if (!result.description && /^[A-Za-z]/.test(line)) { result.description = line  } return result }, {})
+            const result = await octokit.request(`GET ${(new URL(current.url).pathname)}/README.${readmeFormat}`)
+            const parsedReadme = parseReadme(Buffer.from(result.data.content, 'base64').toString(), readmeFormat)
             const md = createMarkDown(currentKey, parsedReadme.name, language, registryType, current.html_url, parsedReadme.description)
             // collector entries are named reverse (collector-{registryTpe}) compared to languages ({registryTpe}-{language}), we fix this here.
             const fileName = language === 'collector' ? `${language}-${registryType}-${currentKey}.md` : `${registryType}-${language}-${currentKey}.md`
@@ -95,14 +114,15 @@ async function createFilesFromScrapeResult(existing, found, settings) {
     })
 }
 
-async function scrapeInstrumentationLibrariesByLanguage(language, path) {
+async function scrapeInstrumentationLibrariesByLanguage(language, path, readmeFormat = 'md') {
     // https://github.com/open-telemetry/opentelemetry-js-contrib/tree/main/plugins/node/
     const keyMapper = x => x.split('-').filter(y => !['opentelemetry', 'instrumentation'].includes(y)).join('-')
     const found = await scrapeNew(path, `opentelemetry-${language}-contrib`, () => true, keyMapper)
-    const existing = scrapeExisting(`instrumentation-${language}`)
+    const existing = await scrapeExisting(`instrumentation-${language}`)
     createFilesFromScrapeResult(existing, found, {
         language,
-        registryType: 'instrumentation'
+        registryType: 'instrumentation',
+        readmeFormat
     })
 }
 
@@ -113,7 +133,8 @@ async function scrapeCollectorComponent(component) {
     const existing = await scrapeExisting(`collector-${component}`, true)
     createFilesFromScrapeResult(existing, found, {
         language: 'collector',
-        registryType: component
+        registryType: component,
+        readmeFormat: 'md'
     })
 }
 
