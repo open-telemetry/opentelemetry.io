c4c8fe76999655e599bc74d8844b63fc17c23c40
svrnm <neumanns@cisco.com>
2022-11-14 18:17:40 +0100

    Fix some bugs in collector check version script
    Signed-off-by: svrnm <neumanns@cisco.com>

--- .github/workflows/scripts/check-collector-version.sh
+++ .github/workflows/scripts/check-collector-version.sh
@@ -2,7 +2,7 @@
 # this uses a lot of the code that already exists in
 # https://github.com/grafana/opentelemetry-collector-components/blob/main/scripts/update-to-latest-otelcol.sh
 
-REPO_DIR="$( cd "$(dirname $( dirname "${BASH_SOURCE[0]}" ))" &> /dev/null && pwd )"
+REPO_DIR=$(realpath $(dirname $0)/../../..)
 
 if ! command -v gh &>/dev/null; then
     echo "The command 'gh' is expected to exist and be configured in order to update to the latest otelcol."
@@ -10,22 +10,15 @@ if ! command -v gh &>/dev/null; then
 fi
 
 # get the latest tag, without the "v" prefix
-latest_core_version=$(gh api -q .tag_name repos/open-telemetry/opentelemetry-collector/releases/latest | sed 's/^v//')
-latest_contrib_version=$(gh api -q .tag_name repos/open-telemetry/opentelemetry-collector-contrib/releases/latest | sed 's/^v//')
+latest_version=$(gh api -q .tag_name repos/open-telemetry/opentelemetry-collector-releases/releases/latest | sed 's/^v//')
 
-# here we don't like having different versions, so we skip creating the PR if contrib & core are not equal
-if [ $latest_core_version != $latest_contrib_version ]; then
-    echo "The contrib and core versions aren't matching. This might be OK, but perhaps there's a release in process?"
-    exit 1
-fi
-
-branch="opentelemetrybot/collector_version_${latest_core_version}"
+branch="opentelemetrybot/collector_version_${latest_version}"
 
 git checkout -b "${branch}" main
 
 gettingStartedFile=${REPO_DIR}/content/en/docs/collector/getting-started.md
 
-sed -i "s/collectorVersion:.*/collectorVersion: ${latest_core_version}/" ${gettingStartedFile}
+sed -i "s/collectorVersion:.*/collectorVersion: ${latest_version}/" ${gettingStartedFile}
 
 if git diff --quiet ${gettingStartedFile}; then
     echo "We are already at the latest versions."
@@ -34,10 +27,10 @@ fi
 
 git add ${gettingStartedFile}
 
-git commit -m "Bump OpenTelemetry collector version to ${latest_core_version}"
+git commit -m "Bump OpenTelemetry collector version to ${latest_version}"
 git push --set-upstream origin "${branch}"
 
 echo "Creating the pull request on your behalf."
 gh pr create --label auto-update,sig:collector \
-             --title "Bump collector version to ${latest_core_version}" \
-             --body "Use OpenTelemetry collector v${latest_core_version} in the collector getting started."
+             --title "Bump collector version to ${latest_version}" \
+             --body "Use OpenTelemetry collector v${latest_version} in the collector getting started."
