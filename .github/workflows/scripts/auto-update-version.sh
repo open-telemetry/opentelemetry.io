17c40e9c976e4571189f4a4313dcedb68f8926bf
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-02-15 20:56:35 -0800

    Auto-update more versions

---
+++ .github/workflows/scripts/auto-update-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+repo=$1
+variable_name=$2
+file_names=("${@:3}") # remaining args
+
+latest_version=$(gh api -q .tag_name "repos/open-telemetry/$repo/releases/latest" | sed 's/^v//')
+
+echo "Repo: $repo"
+echo "Latest version: $latest_version"
+
+for file_name in "${file_names[@]}"
+do
+  sed -i -e "s/$variable_name: .*/$variable_name: $latest_version/" "$file_name"
+done
+
+if git diff --quiet; then
+  echo "Already at the latest version."
+  exit 0
+fi
+
+message="Update $repo version to $latest_version"
+body="Update $repo version to \`$latest_version\`."
+
+existing_pr_count=$(gh pr list --state all --search "in:title $message" | wc -l)
+if [ "$existing_pr_count" -gt 0 ]; then
+    echo "PR for this version was already created."
+    exit 0
+fi
+
+branch="opentelemetrybot/auto-update-$repo-$latest_version"
+
+git checkout -b "$branch"
+git commit -a -m "$message"
+git push --set-upstream origin "$branch"
+
+echo "Creating a pull request on your behalf."
+gh pr create --label auto-update \
+             --title "$message" \
+             --body "$body"
