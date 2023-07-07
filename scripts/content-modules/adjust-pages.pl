d7abb542d46ed189a0135617c99ce356f1b692de
Patrice Chalin <chalin@users.noreply.github.com>
2023-07-07 08:41:31 -0400

    Update semantic-conventions (#2982)

--- scripts/content-modules/adjust-pages.pl
+++ scripts/content-modules/adjust-pages.pl
@@ -14,6 +14,7 @@ my $gD = 0;
 my $otelSpecRepoUrl = 'https://github.com/open-telemetry/opentelemetry-specification';
 my $otlpSpecRepoUrl = 'https://github.com/open-telemetry/opentelemetry-proto';
 my $opAmpSpecRepoUrl = 'https://github.com/open-telemetry/opamp-spec';
+my $semconvSpecRepoUrl = 'https://github.com/open-telemetry/semantic-conventions';
 my $semConvRef = "$otelSpecRepoUrl/blob/main/semantic_conventions/README.md";
 my $specBasePath = '/docs/specs';
 my $path_base_for_github_subdir = "content/en$specBasePath";
@@ -23,7 +24,7 @@ my %versions = qw(
 );
 my $otelSpecVers = $versions{'spec:'};
 my $otlpSpecVers = $versions{'otlp:'};
-my $unused;
+
 # TODO: remove once OpAMP spec has been updated
 my $opampFrontMatter = << "EOS";
 title: Open Agent Management Protocol
@@ -35,6 +36,47 @@ path_base_for_github_subdir:
   from: content/en/docs/specs/opamp/index.md
   to: specification.md
 EOS
+
+# TODO: remove once Semconv spec has been updated
+my $semconvFrontMatter = << "EOS";
+linkTitle: Semantic Conventions
+no_list: true
+cascade:
+  body_class: otel-docs-spec
+  github_repo: &repo $semconvSpecRepoUrl
+  github_subdir: docs
+  path_base_for_github_subdir: content/en/docs/specs/semconv/
+  github_project_repo: *repo
+  draft: true
+EOS
+
+# Adjust semconv title capitalization
+sub toTitleCase($) {
+    my $str = shift;
+    my @specialCaseWords = qw(
+        CloudEvents
+        CouchDB
+        DynamoDB
+        FaaS
+        GraphQL
+        gRPC
+        HBase
+        MongoDB
+        OpenTelemetry
+        RabbitMQ
+        RocketMQ
+    );
+    my %specialCases = map { lc($_) => $_ } @specialCaseWords;
+    while ($str =~ /(\b[A-Z]+\b)/g) {
+        $specialCases{lc $1} = $1;
+    }
+    $str =~ s/(\w+)/\u\L$1/g;
+    while (my ($key, $value) = each %specialCases) {
+        $str =~ s/\b\u\L$key\b/$value/g;
+    }
+    $str =~ s/\b(A|And|As|For|In|On)\b/\L$1/g;
+    return $str;
+}
 
 sub printTitleAndFrontMatter() {
   print "---\n";
@@ -49,10 +91,52 @@ sub printTitleAndFrontMatter() {
     $frontMatterFromFile .= "weight: 20\n" if $frontMatterFromFile !~ /^\s*weight/;
   } elsif ($title eq 'OpAMP: Open Agent Management Protocol') {
     $frontMatterFromFile = $opampFrontMatter unless $frontMatterFromFile;
+  } elsif ($title eq 'OpenTelemetry Semantic Conventions') {
+    $frontMatterFromFile = $semconvFrontMatter unless $frontMatterFromFile;
+  } elsif ($ARGV =~ /tmp\/semconv\/docs/) {
+    $title = toTitleCase($title);
+    $linkTitle = 'Database' if $title =~ /Database Calls and Systems$/i;
+    if ($linkTitle =~ /^Database (.*)$/i) {
+      $linkTitle = "$1";
+    } elsif ($linkTitle =~ /^FaaS (.*)$/i) {
+      $linkTitle = "$1";
+    } elsif ($linkTitle =~ /^HTTP (.*)$/i) {
+      $linkTitle = "$1";
+    } elsif ($linkTitle =~ /^Microsoft (.*)$/i) {
+      $linkTitle = "$1";
+    } elsif ($linkTitle =~ /^RPC (.*)$/i) {
+      $linkTitle = "$1";
+    } elsif ($linkTitle =~ /^(Exceptions|Feature Flags) .. (.*)$/i) {
+      $linkTitle = "$2";
+    }
+    if ($linkTitle =~ /^(.*) Attributes$/i && $title ne 'General Attributes') {
+      $linkTitle = "$1";
+    }
+    $linkTitle = 'Attributes' if $title eq 'General Attributes';
+    $linkTitle = 'Events' if $linkTitle eq 'Event';
+    $linkTitle = 'Logs' if $title =~ /Logs Attributes$/;
+    $linkTitle = 'Connect' if $title =~ /Connect RPC$/;
+    $linkTitle = 'SQL' if $title =~ /SQL Databases$/;
+    $title = 'Semantic Conventions for Function-as-a-Service' if $title eq 'Semantic Conventions for FaaS';
+    $linkTitle = 'Tracing Compatibility' if $linkTitle eq 'Tracing Compatibility Components';
+    if ($title =~ /Semantic Convention\b/) {
+      $title =~ s/Semantic Convention\b/$&s/g;
+    }
   }
   my $titleMaybeQuoted = ($title =~ ':') ? "\"$title\"" : $title;
   print "title: $titleMaybeQuoted\n" if $frontMatterFromFile !~ /title: /;
-  ($unused, $linkTitle) = $title =~ /^OpenTelemetry (Protocol )?(.*)/;
+  printf STDOUT ">1 $title -> $linkTitle\n" if $title =~ /Function/;
+  if ($title =~ /^OpenTelemetry (Protocol )?(.*)/) {
+    $linkTitle = $2;
+  } elsif ($title =~ /^(.*?) Semantic Conventions?$/i && !$linkTitle) {
+    $linkTitle = $1;
+  } elsif ($title =~ /^Semantic Conventions? for (.*)$/i && !$linkTitle) {
+    $linkTitle = $1;
+  }
+  if ($linkTitle =~ /^Function.as.a.Service$/i) {
+    $linkTitle = 'FaaS';
+  }
+  printf STDOUT ">2 $title -> $linkTitle\n" if $title =~ /Function/;
   # TODO: add to front matter of OTel spec file and drop next line:
   $linkTitle = 'Design Goals' if $title eq 'Design Goals for OpenTelemetry Wire Protocol';
   print "linkTitle: $linkTitle\n" if $linkTitle and $frontMatterFromFile !~ /linkTitle: /;
@@ -61,6 +145,13 @@ sub printTitleAndFrontMatter() {
     print "path_base_for_github_subdir:\n";
     print "  from: $path_base_for_github_subdir/otel/$1_index.md\n";
     print "  to: $1README.md\n";
+  } elsif ($ARGV =~ /tmp\/semconv\/docs\/(.*?)_index.md$/) {
+    print "path_base_for_github_subdir:\n";
+    print "  from: $path_base_for_github_subdir/semconv/$1_index.md\n";
+    print "  to: $1README.md\n";
+    if ($linkTitle eq 'General') {
+      print "weight: -1\n";
+    }
   }
   print "---\n";
 }
@@ -84,6 +175,7 @@ while(<>) {
   }
   if(! $title) {
     ($title) = /^#\s+(.*)/;
+    $linkTitle = '';
     printTitleAndFrontMatter() if $title;
     next;
   }
@@ -95,6 +187,16 @@ while(<>) {
   if(/<!-- toc -->/) {
     while(<>) { last if/<!-- tocstop -->/; }
     next;
+  }
+
+  ## Semconv
+
+  if ($ARGV =~ /\/semconv/) {
+    s|(\]\()/docs/|$1$specBasePath/semconv/|g;
+    s|(\]:\s*)/docs/|$1$specBasePath/semconv/|;
+
+    # TODO: drop once semconv pages are fixed:
+    s|(/resource/faas\.md)#function-as-a-service|$1|;
   }
 
   # SPECIFICATION custom processing
