47605c06241f83dba2ccd0a05a7f8ad976a255bf
Patrice Chalin <chalin@users.noreply.github.com>
2023-07-13 08:47:06 -0400

    CI: check-links: enable caching and check for warnings (#3026)

---
+++ scripts/check-build-log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# NOTE: add warning skip patterns, one per line to WARNINGS_SKIP_LIST to
+# temporarily skip known warnings or errors.
+
+BUILD_LOG=tmp/build-log.txt
+WARNINGS_SKIP_LIST=.warnings-skip-list.txt
+
+WARNINGS=`grep -E -ie 'warn(ing)?|error' $BUILD_LOG | grep -v -f $WARNINGS_SKIP_LIST`
+
+if [ -e $BUILD_LOG ]; then
+  if [ -n "$WARNINGS" ]; then
+    echo "WARNINGs or ERRORs found in build log:\n"
+    echo "$WARNINGS\n"
+    exit 1
+  fi
+else
+  echo "INFO: $BUILD_LOG file not found."
+fi
