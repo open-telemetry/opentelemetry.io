7ce0a12773ae25ca6b4e33c65647c99dac72d6a4
Severin Neumann <severin.neumann@altmuehlnet.de>
2023-07-15 15:33:27 +0200

    introduce script

--- .github/workflows/scripts/textlint.sh
+++ .github/workflows/scripts/textlint.sh
@@ -4,6 +4,11 @@ ERRORS=$(npx textlint -f json content/en/*.md | jq -r  '.[] | ( .filePath | sub(
 
 if [ -n "${ERRORS}" ] ; then
     echo "${ERRORS}"
+    echo "This is the lead in sentence for the list" >> "$GITHUB_STEP_SUMMARY"
+    echo "" >> "$GITHUB_STEP_SUMMARY" # this is a blank line
+    echo "- Lets add a bullet point" >> "$GITHUB_STEP_SUMMARY"
+    echo "- Lets add a second bullet point" >> "$GITHUB_STEP_SUMMARY"
+    echo "- How about a third one?" >> "$GITHUB_STEP_SUMMARY"
     exit 1
 fi
 
