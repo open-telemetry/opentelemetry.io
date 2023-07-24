eb02c0c156fe5b53062a2adcc24800dae2503119
svrnm <neumanns@cisco.com>
2023-07-24 17:38:43 +0200

    Add coments to textlint.sh

--- .github/workflows/scripts/textlint.sh
+++ .github/workflows/scripts/textlint.sh
@@ -1,31 +1,32 @@
 #!/bin/bash -e
 
-ERRORS=$(npx textlint -f json content/en/*.md | jq -r  '.[] | ( .filePath | sub(env.PWD + "/"; "")  ) as $fp | .messages[] | .message as $message |{line, file: $fp, column, endLine: .loc.end.line, endColumn: .loc.end.column, title: "textlint terminology error" } | . as $in | keys | map("\(.)=\($in[.])") | join(",") | "::error \(.)::\($message)" ')
+# Run textlint and translate the JSON output into a format that provides file annotations for github PRs
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
+ERRORS=$(npx textlint -f json content/en/*.md | jq -r  '
+    .[] | 
+    ( .filePath | sub(env.PWD + "/"; "")  ) as $fp | 
+    .messages[] | 
+    .message as $message |
+    {line, file: $fp, column, endLine: .loc.end.line, endColumn: .loc.end.column, title: "textlint terminology error" } 
+    | . as $in 
+    | keys 
+    | map("\(.)=\($in[.])") 
+    | join(",") 
+    | "::error \(.)::\($message)" '
+)
 
-
+# Print the errors line by line
 if [[ -n "${ERRORS}" ]] ; then
-    if [[ -z "${GITHUB_ACTIONS}" ]]; then
-        GITHUB_STEP_SUMMARY=$(mktemp)
-    fi
-
     OLDIFS=$IFS
     IFS=$'\n'
     for ERROR in ${ERRORS}; do
         echo "$ERROR"
     done
     IFS=$OLDIFS
-    echo "This is the lead in sentence for the list" >> "${GITHUB_STEP_SUMMARY}"
-    echo "" >> "${GITHUB_STEP_SUMMARY}" # this is a blank line
-    echo "- Lets add a bullet point" >> "${GITHUB_STEP_SUMMARY}"
-    echo "- Lets add a second bullet point" >> "${GITHUB_STEP_SUMMARY}"
-    echo "- How about a third one?" >> "${GITHUB_STEP_SUMMARY}"
 
-    if [[ -z "$GITHUB_ACTIONS" ]]; then
-        cat "${GITHUB_STEP_SUMMARY}"
-        rm "${GITHUB_STEP_SUMMARY}"
-    fi
-
+    # if there are errors the workflow run fails
     exit 1
 fi
 
+# no errors, success!
 exit 0
