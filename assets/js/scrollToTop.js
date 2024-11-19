f9efe041948dde450c7e449b5d95740cc7d967eb
Ezerike Caleb <cezerike@gmail.com>
2024-11-19 16:52:11 +0100

    'format fix'

--- assets/js/scrollToTop.js
+++ assets/js/scrollToTop.js
@@ -1,35 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    let footer = document.querySelector(".td-footer");
-  
-    if (!scrollToTopBtn || !footer) return;
-  
-    function adjustButtonPosition() {
-      let footerTop = footer.getBoundingClientRect().top;
-      let windowHeight = window.innerHeight;
-  
-      if (footerTop < windowHeight + 40) {
-        scrollToTopBtn.style.bottom = windowHeight - footerTop + 40 + "px";
-      } else {
-        scrollToTopBtn.style.bottom = "40px";
-      }
+document.addEventListener('DOMContentLoaded', function () {
+  let scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  let footer = document.querySelector('.td-footer');
+
+  if (!scrollToTopBtn || !footer) return;
+
+  function adjustButtonPosition() {
+    let footerTop = footer.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight;
+
+    if (footerTop < windowHeight + 40) {
+      scrollToTopBtn.style.bottom = windowHeight - footerTop + 40 + 'px';
+    } else {
+      scrollToTopBtn.style.bottom = '40px';
     }
-  
-    window.onscroll = function () {
-      if (
-        document.body.scrollTop > 200 ||
-        document.documentElement.scrollTop > 200
-      ) {
-        scrollToTopBtn.style.display = "block";
-      } else {
-        scrollToTopBtn.style.display = "none";
-      }
-  
-      adjustButtonPosition();
-    };
-  
-    scrollToTopBtn.onclick = function () {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    };
-  });
-  
+  }
+
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+
+    adjustButtonPosition();
+  };
+
+  scrollToTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+});
