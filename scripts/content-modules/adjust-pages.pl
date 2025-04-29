b6e7c8842efb749f27d2a437c44e4ee4e5e165e1
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2025-04-29 12:18:52 +0000

    Update opentelemetry-proto version to v1.6.0

--- scripts/content-modules/adjust-pages.pl
+++ scripts/content-modules/adjust-pages.pl
@@ -25,7 +25,7 @@ my $lineNum;
 my %versionsRaw = # Keyname must end with colons because the auto-version update script expects one
   qw(
     spec: 1.43.0
-    otlp: 1.5.0
+    otlp: 1.6.0
     semconv: 1.32.0
   );
 # Versions map without the colon in the keys
