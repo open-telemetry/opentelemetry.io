42de4de1f3f0f2a1271dc064d2d3706eb22f6fa8
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2025-06-14 10:33:30 +0000

    Update spec submodule to v1.46.0-5-g39619e5

--- scripts/content-modules/adjust-pages.pl
+++ scripts/content-modules/adjust-pages.pl
@@ -24,7 +24,7 @@ my $lineNum;
 
 my %versionsRaw = # Keyname must end with colons because the auto-version update script expects one
   qw(
-    spec: v1.46.0-3-g8f01d12
+    spec: v1.46.0-5-g39619e5
     otlp: 1.7.0
     semconv: 1.34.0
   );
