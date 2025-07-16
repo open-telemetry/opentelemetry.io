f64f6d14afe353be621b1c4c46a2e1d340384591
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2025-07-16 19:48:22 +0000

    Update semantic-conventions version to v1.36.0

--- scripts/content-modules/adjust-pages.pl
+++ scripts/content-modules/adjust-pages.pl
@@ -26,7 +26,7 @@ my %versionsRaw = # Keyname must end with colons because the auto-version update
   qw(
     spec: 1.46.0
     otlp: 1.7.0
-    semconv: 1.35.0
+    semconv: 1.36.0
   );
 # Versions map without the colon in the keys
 my %versions = map { s/://r => $versionsRaw{$_} } keys %versionsRaw;
