01acff812c98324344e3632b93199cf067a0065b
Patrice Chalin <pchalin@gmail.com>
2025-10-29 16:54:12 -0400

    Update adjust-pages.pl

--- scripts/content-modules/adjust-pages.pl
+++ scripts/content-modules/adjust-pages.pl
@@ -26,7 +26,7 @@ my %versionsRaw = # Keyname must end with colons because the auto-version update
   qw(
     spec: 1.50.0
     otlp: 1.8.0
-    semconv: 1.37.0
+    semconv: 1.38.0
   );
 # Versions map without the colon in the keys
 my %versions = map { s/://r => $versionsRaw{$_} } keys %versionsRaw;
