009bc12ec73af90252a443faf18c7f1eb8ebd969
otelbot <197425009+otelbot@users.noreply.github.com>
2026-02-13 17:26:58 +0000

    Update opentelemetry-specification version to v1.54.0

--- scripts/content-modules/adjust-pages.pl
+++ scripts/content-modules/adjust-pages.pl
@@ -24,7 +24,7 @@ my $lineNum;
 
 my %versionsRaw = # Keyname must end with colons because the auto-version update script expects one
   qw(
-    spec: 1.53.0
+    spec: 1.54.0
     otlp: 1.9.0
     semconv: 1.39.0
   );
